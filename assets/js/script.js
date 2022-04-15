8b1584d3ec46e8cd865a70d07aef2f22bad28bfe
Vertis-Z <48894365+Vertis-Z@users.noreply.github.com>
2022-04-14 22:59:22 -0400

    added all character variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,12 @@ var passwordValidation = (userInput) => {
   return true
 };
 
+// array that will contain all the character variables needed
+let numCase = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
+let specCase = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
+let lowCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
